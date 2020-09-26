7292715ba205a630ab1a3e11e223b1ad13af6a16
Arun kumar <arunkumara300@gmail.com>
2020-09-26 19:47:21 +0530

    Netflix - clone - stable base

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log(window.scrollY);
+      if (window.scrollY > 100) {
+        handleshow(true);
+      } else handleshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/FelixINX_Avatar.png/240px-FelixINX_Avatar.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
